d74e85b2abafa3002c82d841a2d9e7ba50d4d1a7
NAKAGAKI Kenji <nakaken0629@gmail.com>
2019-09-29 20:59:04 +0900

    or_2とor_5を追加

---
+++ ch2/or_5.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import chainer
+import chainer.functions as F
+import chainer.links as L
+import chainer.initializer as I
+from chainer import training
+from chainer.training import extensions
+
+
+class MyChain(chainer.Chain):
+    def __init__(self):
+        super(MyChain, self).__init__()
+        with self.init_scope():
+            self.l1 = L.Linear(2, 6)  # 入力２、中間層6
+            self.l2 = L.Linear(6, 3)  # 中間層6、出力3
+            self.l3 = L.Linear(3, 5)  # 中間層3、出力5
+            self.l4 = L.Linear(5, 2)  # 中間層5、出力2
+
+    def __call__(self, x):
+        h1 = F.relu(self.l1(x))
+        h2 = F.relu(self.l2(h1))
+        h3 = F.relu(self.l3(h2))
+        y = self.l4(h3)
+        return y
+
+
+epoch = 100
+batchsize = 4
+
+# データの作成
+trainx = np.array(([0, 0], [0, 1], [1, 0], [1, 1]), dtype=np.float32)
+trainy = np.array([0, 1, 1, 1], dtype=np.int32)
+train = chainer.datasets.TupleDataset(trainx, trainy)
+test = chainer.datasets.TupleDataset(trainx, trainy)
+
+# ニューラルネットワークの登録
+model = L.Classifier(MyChain(), lossfun=F.softmax_cross_entropy)
+# chainer.serializers.load_npz('result/out.model', model)
+optimizer = chainer.optimizers.Adam()
+optimizer.setup(model)
+
+# イテレータの定義
+train_iter = chainer.iterators.SerialIterator(train, batchsize)  # 学習用
+test_iter = chainer.iterators.SerialIterator(
+    test, batchsize, repeat=False, shuffle=False)  # 評価用
+
+# アップデータの登録
+updater = training.StandardUpdater(train_iter, optimizer)
+
+# トレーナーの登録
+trainer = training.Trainer(updater, (epoch, 'epoch'))
+
+# 学習状況の表示や保存
+trainer.extend(extensions.LogReport())  # ログ
+trainer.extend(extensions.Evaluator(test_iter, model))  # エポック数の表示
+trainer.extend(extensions.PrintReport(['epoch', 'main/loss', 'validation/main/loss',
+                                       'main/accuracy', 'validation/main/accuracy', 'elapsed_time']))  # 計算状態の表示
+# trainer.extend(extensions.dump_graph('main/loss')) # ニューラルネットワークの構造
+# trainer.extend(extensions.PlotReport(['main/accuracy', 'validation/main/accuracy'], 'epoch', file_name='accuracy.png')) # 精度のグラフ
+# trainer.extend(extensions.snapshot(), trigger=(100, 'epoch')) # 学習再開のためのスナップショット出力
+# chainer.serializers.load_npz('result/snapshot_iter_500', trainer) # 再開用
+
+# 学習開始
+trainer.run()
+chainer.serializers.save_npz('result/out.model', model)
